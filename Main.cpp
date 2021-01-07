60a8dd74c4d90f93a88976805c88587bf9c9632a
Nikita Rudenko <RudenkoRNK@gmail.com>
2021-01-08 01:20:23 +0600

    Futher improvements

--- Main.cpp
+++ Main.cpp
@@ -6,12 +6,16 @@ int main(int argc, char *argv[]) {
   auto pow = GetIntArgument(argc, argv, 12);
   auto size = static_cast<size_t>(1 << pow);
 
-  auto queue = GetDefaultQueue();
+  auto GPUSelector = cl::sycl::gpu_selector{};
+  auto queue = cl::sycl::queue{GPUSelector};
+  PrintInfo(queue, std::cout);
+
   auto vec = GetRandomVector(size);
 
+  WarmUp(queue);
+
   Check(
-      vec, "Warming up GPU", [&](auto &v) { BitonicSortNaive(queue, v); },
-      "CPU", [&](auto &v) { std::sort(v.begin(), v.end()); }, "GPU naive",
+      vec, "CPU", [&](auto &v) { std::sort(v.begin(), v.end()); }, "GPU naive",
       [&](auto &v) { BitonicSortNaive(queue, v); }, "GPU with local memory",
       [&](auto &v) { BitonicSortLocal(queue, v); });
 }
--- Utils.hpp
+++ Utils.hpp
@@ -10,14 +10,6 @@
 
 #include <Utility/Misc.hpp>
 
-static void PrintInfo(cl::sycl::queue const &queue, std::ostream &os) {
-  auto device = queue.get_device();
-  os << device.get_info<cl::sycl::info::device::name>() << "\n";
-  os << "Driver version: "
-     << device.get_info<cl::sycl::info::device::driver_version>() << "\n";
-  os << device.get_info<cl::sycl::info::device::opencl_c_version>() << "\n";
-}
-
 static int GetIntArgument(int argc, char *argv[], int defaultValue = 0,
                           size_t nArg = 0) {
   auto index = nArg + 1;
@@ -25,13 +17,6 @@ static int GetIntArgument(int argc, char *argv[], int defaultValue = 0,
     return defaultValue;
   auto arg = std::string(argv[index]);
   return std::stoi(arg);
-}
-
-static cl::sycl::queue GetDefaultQueue() {
-  auto GPUSelector = cl::sycl::gpu_selector{};
-  auto queue = cl::sycl::queue{GPUSelector};
-  PrintInfo(queue, std::cout);
-  return queue;
 }
 
 static std::vector<cl::sycl::cl_int> GetRandomVector(size_t size) {
@@ -42,6 +27,67 @@ static std::vector<cl::sycl::cl_int> GetRandomVector(size_t size) {
   for (auto i = size_t{0}; i < size; ++i)
     vec[i] = rand(gen);
   return vec;
+}
+
+static std::string ToString(cl::sycl::info::device_type deviceType) {
+  using namespace cl::sycl;
+  switch (deviceType) {
+  case info::device_type::cpu:
+    return "cpu";
+  case info::device_type::gpu:
+    return "gpu";
+  case info::device_type::accelerator:
+    return "accelerator";
+  case info::device_type::all:
+    return "all";
+  default:
+    assert(false);
+  }
+}
+
+static void PrintInfo(cl::sycl::queue const &queue, std::ostream &os) {
+  using namespace cl::sycl;
+  auto device = queue.get_device();
+  os << device.get_info<info::device::name>() << std::endl;
+  os << "Driver version: " << device.get_info<info::device::driver_version>()
+     << std::endl;
+  os << "Compute units: " << device.get_info<info::device::max_compute_units>()
+     << std::endl;
+  os << "Work group size: "
+     << device.get_info<info::device::max_work_group_size>() << std::endl;
+  os << "Max clock frequency: "
+     << device.get_info<info::device::max_clock_frequency>() << " MHz\n";
+  os << "Total memory : "
+     << (device.get_info<info::device::global_mem_size>() / (1024 * 1024))
+     << " MiB" << std::endl;
+  os << "Addressing model: " << device.get_info<info::device::address_bits>()
+     << " bit" << std::endl;
+  os << "OpenCL profile: " << device.get_info<info::device::profile>()
+     << std::endl;
+  os << "OpenCL version: " << device.get_info<info::device::opencl_c_version>()
+     << std::endl;
+  auto extensions = device.get_info<info::device::extensions>();
+  os << "Supported extensions: " << std::endl;
+  for (auto const &ext : extensions)
+    os << "  * " << ext << std::endl;
+
+  os << std::endl;
+}
+
+static void WarmUp(cl::sycl::queue &queue) {
+  auto time = Utility::Benchmark([&]() {
+    auto x = std::vector<cl::sycl::cl_int>(8);
+    auto buf = cl::sycl::buffer{x};
+    auto range1d = cl::sycl::range<1>{buf.get_count()};
+    queue.submit([&](cl::sycl::handler &h) {
+      auto access =
+          buf.template get_access<cl::sycl::access::mode::read_write>(h);
+      h.parallel_for<class WarmUpKernel>(
+          range1d, [access](cl::sycl::id<1> id) { access[id[0]] = id[0]; });
+    });
+  });
+  std::cout << "GPU warm up time: " << (time.count() / 1000) << " microseconds"
+            << std::endl;
 }
 
 template <typename T, typename Competitor>
