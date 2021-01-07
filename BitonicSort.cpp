0c7bb584e7bd91ae5bbabcf6a5b6ff43560c946f
Nikita Rudenko <RudenkoRNK@gmail.com>
2021-01-07 21:10:48 +0600

    Update BitonicSort

--- BitonicSort.cpp
+++ BitonicSort.cpp
@@ -4,26 +4,19 @@
 #include <cassert>
 #include <random>
 
-// using namespace sycl::INTEL::gpu;
-// using namespace cl::sycl;
-// void kernel(accessor<int, 1, access::mode::read_write,
-//                     access::target::global_buffer> &buf)
-//    __attribute__((sycl_device)) {
-//  simd<uint32_t, 32> offsets(0, 1);
-//  simd<int, 32> v1(0, 1);
-//
-//  auto v0 = gather<int, 32>(buf.get_pointer(), offsets);
-//
-//  v0 = v0 + v1;
-//
-//  scatter<int, 32>(buf.get_pointer(), v0, offsets);
-//}
+void PrintInfo(cl::sycl::queue const &queue, std::ostream &os) {
+  auto device = queue.get_device();
+  os << device.get_info<cl::sycl::info::device::name>() << "\n";
+  os << "Driver version: "
+     << device.get_info<cl::sycl::info::device::driver_version>() << "\n";
+  os << device.get_info<cl::sycl::info::device::opencl_c_version>() << "\n";
+}
 
-static std::vector<cl::sycl::cl_int> GetRandomVector(size_t size) {
+std::vector<cl::sycl::cl_int> GetRandomVector(size_t size) {
   auto rd = std::random_device{};
   auto gen = std::mt19937{static_cast<unsigned>(rd())};
   auto vec = std::vector<cl::sycl::cl_int>(size);
-  auto rand = std::uniform_int_distribution<>(0, 1000);
+  auto rand = std::uniform_int_distribution<>();
   for (auto i = size_t{0}; i < size; ++i)
     vec[i] = rand(gen);
   return vec;
@@ -31,7 +24,7 @@ static std::vector<cl::sycl::cl_int> GetRandomVector(size_t size) {
 
 // Copy-paste https://stackoverflow.com/a/14880868/8099151
 // The 'i' is for int, there is a log2 for double in stdclib
-static unsigned int log2i(unsigned int x) {
+unsigned int log2i(unsigned int x) {
   unsigned int log2Val = 0;
   // Count push off bits to right until 0
   // 101 => 10 => 1 => 0
@@ -43,11 +36,11 @@ static unsigned int log2i(unsigned int x) {
   return log2Val;
 }
 
-template <typename T> void BitonicSort(std::vector<T> &vec) {
+template <typename T>
+void BitonicSort(cl::sycl::queue &queue, std::vector<T> &vec) {
   auto size = vec.size();
   auto nLargeSteps = static_cast<cl::sycl::cl_int>(log2i(size));
   auto buf = cl::sycl::buffer{vec};
-  auto queue = cl::sycl::queue{};
   auto range1d = cl::sycl::range<1>{buf.get_count() / 2};
 
   for (auto i = cl::sycl::cl_int{0}; i != nLargeSteps; ++i)
@@ -72,18 +65,27 @@ template <typename T> void BitonicSort(std::vector<T> &vec) {
 }
 
 int main() {
-  auto size = size_t{4096};
+  auto size = size_t{16777216};
+
+  auto GPUSelector = cl::sycl::gpu_selector{};
+  auto queue = cl::sycl::queue{GPUSelector};
+  PrintInfo(queue, std::cout);
+  auto antiDCE = 0;
+
+  auto gpu_time = Utility::Benchmark([&]() {
+    auto vec = GetRandomVector(size);
+    BitonicSort(queue, vec);
+    antiDCE += vec[size - 1];
+  });
+  auto cpu_time = Utility::Benchmark([&]() {
+    auto vec = GetRandomVector(size);
+    std::sort(vec.begin(), vec.end());
+    antiDCE += vec[size - 1];
+  });
+
   auto vec = GetRandomVector(size);
   auto sortedVec = vec;
-
-  auto gpu_time = Utility::Benchmark(
-      [&]() {
-        auto vec = GetRandomVector(size);
-        BitonicSort(vec);
-      },
-      10);
-
-  BitonicSort(vec);
+  BitonicSort(queue, vec);
   std::sort(sortedVec.begin(), sortedVec.end());
   for (auto i = size_t{0}; i < size; ++i) {
     if (vec[i] != sortedVec[i]) {
@@ -94,5 +96,10 @@ int main() {
     }
   }
   std::cout << "Sorted successfully!" << std::endl;
-  std::cout << "GPU time: " << gpu_time.count() << " nanoseconds" << std::endl;
+  std::cout << "GPU time: " << (gpu_time.count() / 1000) << " microseconds"
+            << std::endl;
+  std::cout << "CPU time: " << (cpu_time.count() / 1000) << " microseconds"
+            << std::endl;
+
+  std::cout << "Anti-DCE: " << antiDCE << std::endl;
 }
