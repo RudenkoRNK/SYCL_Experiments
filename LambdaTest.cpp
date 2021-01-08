c82f23f0b75659eebc7bbf80b960aa13f374db42
Nikita Rudenko <RudenkoRNK@gmail.com>
2021-01-08 21:44:48 +0600

    Amend LambdaTest so it will be easier to read LLVM IR

--- LambdaTest.cpp
+++ LambdaTest.cpp
@@ -1,39 +1,45 @@
 #include <CL/sycl.hpp>
 
-void Fill(std::vector<int> &vec, size_t workGroupSize, bool useLambda) {
+void FillLambda(cl::sycl::queue &queue, std::vector<int> &vec,
+                size_t workGroupSize) {
   using namespace cl::sycl;
-  using GlobalAccess =
-      accessor<int, 1, access::mode::read_write, access::target::global_buffer>;
-
-  auto GPUSelector = gpu_selector{};
-  auto queue = cl::sycl::queue{GPUSelector};
   auto buf = buffer{vec};
   auto nWorkGroups = vec.size() / workGroupSize;
 
   queue.submit([&](handler &h) {
     auto global = buf.template get_access<access::mode::read_write>(h);
-    auto lambda = [=](GlobalAccess global, group<1> g) {
-      g.parallel_for_work_item([=](h_item<1> it) {
+    auto lambda = [=](group<1> g) {
+      g.parallel_for_work_item(range<1>{4}, [=](h_item<1> it) {
         global[it.get_global_id()[0]] = it.get_local_id()[0];
       });
     };
 
-    if (useLambda) {
-      h.parallel_for_work_group<class LambdaTestKernel>(
-          range<1>{nWorkGroups}, range<1>{workGroupSize},
-          [=](group<1> g) { lambda(global, g); });
-    } else {
-      h.parallel_for_work_group<class WithoutLambdaTestKernel>(
-          range<1>{nWorkGroups}, range<1>{workGroupSize}, [=](group<1> g) {
-            g.parallel_for_work_item([=](h_item<1> it) {
-              global[it.get_global_id()[0]] = it.get_local_id()[0];
-            });
+    h.parallel_for_work_group<class LambdaTestKernel>(
+        range<1>{nWorkGroups}, range<1>{workGroupSize},
+        [=](group<1> g) { lambda(g); });
+  });
+
+  queue.wait();
+  buf.template get_access<cl::sycl::access::mode::read_write>();
+}
+
+void FillWithoutLambda(cl::sycl::queue &queue, std::vector<int> &vec,
+                       size_t workGroupSize) {
+  using namespace cl::sycl;
+  auto buf = buffer{vec};
+  auto nWorkGroups = vec.size() / workGroupSize;
+
+  queue.submit([&](handler &h) {
+    auto global = buf.template get_access<access::mode::read_write>(h);
+    h.parallel_for_work_group<class WithoutLambdaTestKernel>(
+        range<1>{nWorkGroups}, range<1>{workGroupSize}, [=](group<1> g) {
+          g.parallel_for_work_item([=](h_item<1> it) {
+            global[it.get_global_id()[0]] = it.get_local_id()[0];
           });
-    }
+        });
   });
   queue.wait();
   buf.template get_access<cl::sycl::access::mode::read_write>();
-  std::sort(vec.begin(), vec.end());
 }
 
 int main() {
@@ -44,8 +50,14 @@ int main() {
   auto lambdaVec = emptyVec;
   auto withoutLambdaVec = emptyVec;
 
-  Fill(lambdaVec, workGroupSize, true);
-  Fill(withoutLambdaVec, workGroupSize, false);
+  auto GPUSelector = cl::sycl::gpu_selector{};
+  auto queue = cl::sycl::queue{GPUSelector};
+
+  FillLambda(queue, lambdaVec, workGroupSize);
+  FillWithoutLambda(queue, withoutLambdaVec, workGroupSize);
+
+  std::sort(lambdaVec.begin(), lambdaVec.end());
+  std::sort(withoutLambdaVec.begin(), withoutLambdaVec.end());
   for (auto i = 0; i != size; ++i)
     std::cout << "Expected: " << i / nWorkGroups
               << "; Computed without lambda: " << withoutLambdaVec[i]
