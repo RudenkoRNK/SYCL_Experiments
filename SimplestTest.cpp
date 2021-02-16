380fa4d2052f281653095587cac138227dada5ad
Nikita Rudenko <RudenkoRNK@gmail.com>
2021-02-16 16:06:01 +0600

    Add simplest test

---
+++ SimplestTest.cpp
@@ -0,0 +1,23 @@
+#include <CL/sycl.hpp>
+
+int main() {
+  using namespace cl::sycl;
+  auto size = size_t{16};
+  auto vec = std::vector<int>(size);
+
+  auto GPUSelector = gpu_selector{};
+  auto q = queue{GPUSelector};
+  auto buf = buffer{vec};
+    
+  q.submit([&](handler &h) {
+    auto access = buf.template get_access<access::mode::read_write>(h);
+    h.parallel_for<class SimplestKernel>(range<1>{size}, [=](id<1> id) {
+      access[id[0]] = id[0];
+    });
+  });
+  q.wait();
+  buf.template get_access<access::mode::read_write>();
+
+  for (auto i = 0; i != size; ++i)
+    std::cout << "Index: " << i << "; Result: " << vec[i] << std::endl;
+}
